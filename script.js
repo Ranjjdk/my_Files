d76e59404a075e587c9f1128e090af6820becef9
Ranjjdk <108882618+Ranjjdk@users.noreply.github.com>
2023-11-22 18:59:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const inputText=document.getElementById("input-text");
+const listContainer=document.getElementById("list-container");
+
+
+function addTask() {
+
+     if(inputText.value===''){
+ 
+        alert("You must write something");
+     }else{
+
+          let li=document.createElement("li");
+           li.innerHTML=inputText.value;
+           listContainer.appendChild(li);
+            
+         let span=document.createElement('span')
+             
+             span.innerHTML='\u00d7';
+             li.appendChild(span);
+     }
+        inputText.value='';
+
+     
+
+}
